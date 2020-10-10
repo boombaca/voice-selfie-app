3816ace4c43fe2ce847c5aca252f35c7ad2e2c8e
aaryan <66792777+boombaca@users.noreply.github.com>
2020-10-10 20:32:02 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,8 @@
+function english()
+{
+    window.location = "index2.html";
+}
+function hindi()
+{
+    window.location = "index3.html";
+}
---
+++ mainhindi.js
@@ -0,0 +1,77 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="Mera selfie lo")
+      {
+        console.log("ले रहे है सेल्फी ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "das second mae selfie lae rahha hoo . das . no . aatth . saath . chhae . paach . char . ttheen . dao . aek . cashak";
+
+    
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        
+    }, 16500);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+  
+}
+
+function off()
+{
+location.reload();
+}
+
